1611d2d98f05733f152df09d61ef672214edf063
Tyuiu.ShananinaVV@outlook.com <valeriya.v.2508@gmail.com>
2023-11-25 18:32:01 +0500

    Task6

---
+++ Tyuiu.ShananinaVV.Sprint5.Task6.V14.Lib/DataService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.ShananinaVV.Sprint5.Task6.V14.Lib
+{
+    public class DataService : ISprint5Task6V14
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    for (int i =0; i < line.Length; i++ )
+                    {
+                        if ((line[i] == '!') || (line[i] == ',') || (line[i] == '.') || (line[i] == ':') ||
+                            (line[i] == '?') || (line[i] == ';') || (line[i] == ')') || (line[i] == '(') ||
+                            (line[i] == '-') || (line[i] == '"'))
+                        {
+                            count++;
+                        }
+                        
+                    }
+                }
+
+            }
+            return count;
+        }
+    }
+}
